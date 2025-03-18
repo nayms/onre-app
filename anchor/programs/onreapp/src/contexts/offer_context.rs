cf68be7b0959cd21b833b35c06c8b173a224b05b
Marian-Daniel Rolník <marian-danielrolnik@MarianDnielsMBP.localdomain>
2025-03-18 13:09:21 +0100

    f/CLXSP-195 - Optimize imports

--- anchor/programs/onreapp/src/contexts/offer_context.rs
+++ anchor/programs/onreapp/src/contexts/offer_context.rs
@@ -1,6 +1,6 @@
-use anchor_lang::prelude::*;
-use anchor_spl::token::{Token, TokenAccount};
 use crate::state::Offer;
+use anchor_lang::prelude::*;
+use anchor_spl::token::Token;
 
 pub trait CloseOfferContext<'info> {
   fn token_program(&self) -> &Program<'info, Token>;
