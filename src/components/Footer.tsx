11cb06ca89417e19272ac006096f4e0bd8dd763c
Marko Dumic <marko@nayms.com>
2025-03-19 16:32:10 +0100

    Remove some mock data. Update footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
 import React, { PropsWithChildren } from 'react';
 import styled, { css } from 'styled-components';
-import linkedinUrl from '@/assets/icon-linkedin.svg';
-import twitterUrl from '@/assets/icon-x.svg';
-import telegramUrl from '@/assets/icon-telegram.svg';
-import githubUrl from '@/assets/icon-github.svg';
-import mediumUrl from '@/assets/icon-medium.svg';
+// import linkedinUrl from '@/assets/icon-linkedin.svg';
+// import twitterUrl from '@/assets/icon-x.svg';
+// import telegramUrl from '@/assets/icon-telegram.svg';
+// import githubUrl from '@/assets/icon-github.svg';
+// import mediumUrl from '@/assets/icon-medium.svg';
 
 const FooterContainer = styled.div`
   position: relative;
   height: 100%;
   width: 100%;
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
+  display: flex;
+  justify-content: center;
   grid-gap: 32px;
   align-items: center;
   ${({ theme: { color, font } }) => css`
@@ -30,6 +30,7 @@ const NaymsLinks = styled.div`
 
 const Copyright = styled.span``;
 
+// @ts-ignore
 const SocialLinks = styled.div`
   display: flex;
   gap: 16px;
@@ -58,7 +59,7 @@ const ExternalLink: React.FC<PropsWithChildren<ExternalLinkProps>> = ({ icon, hr
 };
 
 export const Footer = () => {
-  const copyrightLine = `©Nayms ${new Date().getFullYear()}`;
+  const copyrightLine = `©OnRe ${new Date().getFullYear()}`;
 
   return (
     <FooterContainer>
@@ -69,13 +70,13 @@ export const Footer = () => {
 
       <Copyright>{copyrightLine}</Copyright>
 
-      <SocialLinks>
-        <ExternalLink icon={linkedinUrl} href="https://linkedin.com/company/nayms" />
-        <ExternalLink icon={twitterUrl} href="https://twitter.com/nayms" />
-        <ExternalLink icon={telegramUrl} href="https://t.me/naymscommunity" />
-        <ExternalLink icon={mediumUrl} href="https://medium.com/nayms" />
-        <ExternalLink icon={githubUrl} href="https://github.com/nayms" />
-      </SocialLinks>
+      {/*<SocialLinks>*/}
+      {/*  <ExternalLink icon={linkedinUrl} href="https://linkedin.com/company/nayms" />*/}
+      {/*  <ExternalLink icon={twitterUrl} href="https://twitter.com/nayms" />*/}
+      {/*  <ExternalLink icon={telegramUrl} href="https://t.me/naymscommunity" />*/}
+      {/*  <ExternalLink icon={mediumUrl} href="https://medium.com/nayms" />*/}
+      {/*  <ExternalLink icon={githubUrl} href="https://github.com/nayms" />*/}
+      {/*</SocialLinks>*/}
     </FooterContainer>
   );
 };
